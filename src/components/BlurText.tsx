48f41132219076b86dc45919af07fe2bed1410b1
Solware-Agency <solwareve@gmail.com>
2025-06-05 18:32:51 -0400

    Updated BlurText.tsx

--- src/components/BlurText.tsx
+++ src/components/BlurText.tsx
@@ -95,7 +95,7 @@ const BlurText: React.FC<BlurTextProps> = ({
   );
 
   return (
-    <p ref={ref} className={`blur-text ${className} flex flex-wrap`}>
+    <p ref={ref} className={`blur-text ${className} flex flex-wrap justify-center`}>
       {elements.map((segment, index) => {
         const animateKeyframes = buildKeyframes(fromSnapshot, toSnapshots);
 
