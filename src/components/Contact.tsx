e19e36f0f7d621a8810633bd35405d928fb5dfc7
Taliii777 <elthon.ps4@gmail.com>
2025-07-14 18:43:48 -0400

    Add files via upload

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -134,7 +134,7 @@ const Contact: React.FC = () => {
 	}, [])
 
 	return (
-		<section className="py-24 bg-gray-50 dark:bg-gray-900 transition-colors duration-300" id="contacto">
+		<section className="py-20 pb-12 bg-gray-50 dark:bg-gray-900 transition-colors duration-300" id="contacto">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="text-center max-w-3xl mx-auto mb-16">
 					<BlurText
